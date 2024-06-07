5c565f067acd0e5f94b5b80bcaf0725316f0e73e
N1hron <denik8008@gmail.com>
2024-06-07 19:09:08 +0300

    initial
    initial

---
+++ templates/default/TemplateName.tsx
@@ -0,0 +1,9 @@
+import styles from './TemplateName.scss';
+
+interface TemplateNameProps { }
+
+export const TemplateName = ({ }: TemplateNameProps) => (
+  <div className={styles.templateName}>
+    TemplateName Component
+  </div>
+);
